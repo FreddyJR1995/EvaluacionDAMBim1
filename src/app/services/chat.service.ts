5a432aebf7b803ef785461250899e2a766e1bded
FreddyJR1995 <56648676+FreddyJR1995@users.noreply.github.com>
2021-01-16 15:10:03 -0500

    Evalacion

---
+++ src/app/services/chat.service.ts
@@ -0,0 +1,112 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import  firebase from 'firebase/app';
+import { switchMap, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import * as CryptoJS from 'crypto-js';
+
+export interface User{
+  uid: string;
+  email: string;
+}
+
+export interface Message{
+  createdAt: firebase.firestore.FieldValue;
+  id:string;
+  from:string;
+  msg:string;
+  fromName:string;
+  myMsg: boolean;
+}
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+  currentUser: User=null;
+  passEncrypt="abcd";
+  constructor(
+    private afAuth:AngularFireAuth, private afs:AngularFirestore
+  ) {
+
+    this.afAuth.onAuthStateChanged((user)=>{
+      this.currentUser=user;
+    });
+   }
+
+   async signUp({email,password}):Promise<any>{
+     const credential= await this.afAuth.createUserWithEmailAndPassword(
+       email,
+       password
+     );
+
+     const uid=credential.user.uid;
+
+     return this.afs.doc(
+      `users/${uid}`
+     ).set({
+       uid,
+       email: credential.user.email,
+     })
+   }
+
+   signIn({email,password}){
+     return this.afAuth.signInWithEmailAndPassword(email,password);
+   }
+
+   signOut():Promise<void>{
+     return this.afAuth.signOut();
+   }
+
+   addChatMessage(msg) {
+     console.log(msg);
+    console.log(this.encryptMsg(msg));
+    return this.afs.collection('messages').add({
+      msg: this.encryptMsg(msg),
+      from: this.currentUser.uid,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp()
+    });
+  }
+  
+  getChatMessages() {
+    let users = [];
+    return this.getUsers().pipe(
+      switchMap(res => {
+        users = res;
+        return this.afs.collection('messages', ref => ref.orderBy('createdAt')).valueChanges({ idField: 'id' }) as Observable<Message[]>;
+      }),
+      map(messages => {
+        // Get the real name for each user
+        for (let m of messages) {          
+          m.fromName = this.getUserForMsg(m.from, users);
+          m.myMsg = this.currentUser.uid === m.from;
+          m.msg=this.decryptMsg(m.msg);
+        }   
+        console.log(messages);
+        return messages;
+      })
+    )
+  }
+
+   private getUsers() {
+    return this.afs.collection('users').valueChanges({ idField: 'uid' }) as Observable<User[]>;
+  }
+
+   private getUserForMsg(msgFromId, users: User[]): string {    
+    for (let usr of users) {
+      if (usr.uid == msgFromId) {
+        return usr.email;
+      }
+    }
+    return 'Deleted';
+    }
+  private encryptMsg(msgDecrypt:string){
+      return CryptoJS.AES.encrypt(msgDecrypt,this.passEncrypt.trim()).toString();
+  }
+  private decryptMsg(msgEncrypt:string){
+      return CryptoJS.AES.decrypt(msgEncrypt,this.passEncrypt.trim()).toString(CryptoJS.enc.Utf8);
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBBIXNFZK32HNAjCSTDVq0f8vbP7TBb5Vs",
+    authDomain: "evaluacion-2bddb.firebaseapp.com",
+    projectId: "evaluacion-2bddb",
+    storageBucket: "evaluacion-2bddb.appspot.com",
+    messagingSenderId: "199181417411",
+    appId: "1:199181417411:web:eb89bd328abb2ac46adb76",
+    measurementId: "G-W5Z6X26TE7"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
